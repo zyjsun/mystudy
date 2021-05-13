44dc9a65fbe9ac5a15978c21b628a9341acf030b
zyjsun <1821554162@qq.com>
2021-05-13 10:45:32 +0800

    shopping

--- LeetCode/base/最佳/回文数/回文数.js
+++ LeetCode/base/最佳/回文数/回文数.js
@@ -1,17 +1,26 @@
-        function ListNode(val, next) {
-         this.val = (val===undefined ? 0 : val)
-         this.next = (next===undefined ? null : next)
+        // function ListNode(val, next) {
+        //  this.val = (val===undefined ? 0 : val)
+        //  this.next = (next===undefined ? null : next)
+        // }
+        //  var isPalindrome = function(head) {
+        //     const vals = [];
+        //     while (head !== null) {
+        //         vals.push(head.val);
+        //         head = head.next;
+        //     }
+        //     for (let i = 0, j = vals.length - 1; i < j; ++i, --j) {
+        //         if (vals[i] !== vals[j]) {
+        //             return false;
+        //         }
+        //     }
+        //     return true;
+        // };
+        let x=-121
+        console.log(typeof(x))
+        let y=x.toString()
+        console.log(typeof(y))
+        console.log(y.length)
+        for(let i=0;i<y.length;i++){
+            console.log(y[i])
         }
-         var isPalindrome = function(head) {
-            const vals = [];
-            while (head !== null) {
-                vals.push(head.val);
-                head = head.next;
-            }
-            for (let i = 0, j = vals.length - 1; i < j; ++i, --j) {
-                if (vals[i] !== vals[j]) {
-                    return false;
-                }
-            }
-            return true;
-        };
+        console.log(y)
