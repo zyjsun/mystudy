43fbf0762b05afc5d148e5c66923198dc2867c11
zyjsun <1821554162@qq.com>
2021-05-23 21:39:00 +0800

    zz

--- js/js执行机制/4.js
+++ js/js执行机制/4.js
@@ -1,38 +1,38 @@
-// function bar(){
-//     console.log(myName,this);//11
-// }
-// //Outer 外部变量 先找执行上下文
-// function foo(){
-//    var myName='22'
-//    bar()//指向全局
-//    console.log(myName)
-// }
-// var myName='11'//变量环境Out指向，全局执行
-// foo()
+function bar(){
+    console.log(myName,this);//11
+}
+//Outer 外部变量 先找执行上下文
+function foo(){
+   var myName='22'
+   bar()//指向全局
+   console.log(myName)
+}
+var myName='11'//变量环境Out指向，全局执行
+foo()
 
 
-// function bar() {
-//     var myName = '潭子'
-//     let test1 = 100
-//     if (1) {
-//       let myName = '浩楠'
-//       console.log(test);
-//     }
-//   }
+function bar() {
+    var myName = '潭子'
+    let test1 = 100
+    if (1) {
+      let myName = '浩楠'
+      console.log(test);
+    }
+  }
   
-//   function foo() {
-//     var myName = '永涛'
-//     let test = 2
-//     {
-//       let test = 3
-//       bar()
-//     }
-//   }
+  function foo() {
+    var myName = '永涛'
+    let test = 2
+    {
+      let test = 3
+      bar()
+    }
+  }
   
-//   var myName = '子贤'
-//   let myAge = 10
-//   let test = 1
-//   foo()
+  var myName = '子贤'
+  let myAge = 10
+  let test = 1
+  foo()
 
 function foo(){
     var myName='杨总'
