60e42bb3b9dfc52ef11091798cfe5f50065965c6
zyjsun <1821554162@qq.com>
2021-06-04 21:34:39 +0800

    zz

--- webpack/use-webpack/src/index.js
+++ webpack/use-webpack/src/index.js
@@ -1,9 +1,15 @@
-// npm i webpack webpack-cli --save-dev
-
+// npm i webpack webpack-cli -g --save-dev
+//  npm i style-loader css-loader --save-dev
+// npm i babel-loader @babel/preset-env @babel/core -D
+import style from './index.css'
 function comp(){
     const el=document.createElement('div')
     el.innerHTML='<i>Hello World</i>'
+    console.log(style);
     return el
 }
+
+console.log([1,2,3].findIndex(x=>x===4));
+console.log('abc'.padStart(10))//填充空格
 
 document.body.appendChild(comp())
--- webpack/use-webpack/webpack.config.js
+++ webpack/use-webpack/webpack.config.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const path=require('path')
 
 module.exports = {
@@ -6,6 +7,26 @@ module.exports = {
     output: {
         filename:'[name].[contenthash].js',
         path: path.resolve(__dirname,'dist')//绝对路径
-
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader']
+            },{
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                type: 'asset'
+            },{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js','.vue'], // import test from '@/index.js'
+        alias: {
+            '@': path.join(__dirname,'src')
+        }
     }
   };
