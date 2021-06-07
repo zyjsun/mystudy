20129980a89876f0d344555e9f2d3bbb163bfd60
zyjsun <1821554162@qq.com>
2021-06-07 20:18:34 +0800

    stack

--- LeetCode/base/数据结构/arr.js
+++ LeetCode/base/数据结构/arr.js
@@ -1,13 +1,16 @@
 
-const arr=new Array(7).fill(1)
+// const arr=new Array(7).fill(1)
 
-console.log(arr)
-arr.forEach(Element=>{
-    console.log(Element)
-})
+// console.log(arr)
+// arr.forEach(Element=>{
+//     console.log(Element)
+// })
 
+const arr2=new Array(7).fill([])
 const arr2=[
     [1,2,3,4,5],
     [1,2,3,4,5],
     [1,2,3,4,5]
 ]
+//slice 不影响原数组 splice影响
+//都返回你截取的那一段
