99bffd41589cd2a72f5cda0d47ff3054d2ee7399
zyjsun <1821554162@qq.com>
2021-06-09 14:35:48 +0800

    stack

--- LeetCode/base/数据结构/stack.js
+++ LeetCode/base/数据结构/stack.js
@@ -1,1 +1,18 @@
-const arr=[1,2]
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+//每日温度
+var dailyTemperatures = function(T) {
+   const len=T.length
+   const stack=[]
+   const res=new Array(len).fill(0)
+    //打破递减关系 记录下标
+   for(let i=0;i<len;i++){
+       while(stack.length&&T[i]>T[stack[stack.length-1]]){
+        const top=stack.pop();//栈顶元素
+        res[top]=i-top;//规避了无意义的操作
+       }
+        stack.push(i)
+   }
+
+   return res;
+}
+
--- ruanyf/es6/set-map/weakMap.js
+++ ruanyf/es6/set-map/weakMap.js
@@ -3,3 +3,5 @@ const key={foo:1}
 const k1=[1,2,3]
 const wm=new WeakMap([[k1,foo]])
 console.log(wm)
+
+
