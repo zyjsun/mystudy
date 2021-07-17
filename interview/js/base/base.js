291e01af92e4fcd6efafdd21780bbaeb73626b8c
zyjsun <1821554162@qq.com>
2021-07-17 15:40:31 +0800

    js

--- interview/js/base/base.js
+++ interview/js/base/base.js
@@ -14,9 +14,9 @@
 //   console.log(p1);//{name:'胡总'，age:'20'}
 //   console.log(p2);//{name:'袁总'，age:'30'}
 
-const Person=function(){}
+// const Person=function(){}
 
-const p1=new Person()
+// const p1=new Person()
 
 // console.log(p1 instanceof Person) true
 
@@ -29,3 +29,15 @@ const p1=new Person()
 // 4+[1,2,3]// '41,2,3'
 
 //'a' +(+'b')  //'aNaM'
+
+function getType (obj) {
+  let type = typeof obj;
+  if (type !== "object") {    // 先进行typeof判断，如果是基础数据类型，直接返回
+    return type;
+  }
+  // 对于typeof返回结果是object的，再进行如下的判断，正则返回结果
+  return Object.prototype.toString.call(obj)
+  // .replace(/^\[object (\S+)\]$/, '$1');  // 注意正则中间有个空格
+}
+
+console.log(getType([1, 2, 3, 4]))
