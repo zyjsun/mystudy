0a3698829afdb0d9ca16cb1edbd6f80a108a9746
zyjsun <1821554162@qq.com>
2021-07-23 08:45:30 +0800

    interview

---
+++ interview/字符串方法/string.js
@@ -0,0 +1,24 @@
+// charAt(x) 返回字符串中x位置的字符，下标从 0 开始
+// charCodeAt(x) 返回字符串中x位置处字符的unicode值
+// concat(v1,v2..)用于于连接两个或多个字符串，此方法不改变现有的字符串，返回拼接后的新的字符串
+// fromCharcode(c1,c2) 转换一组Unicode值转换为字符。
+// indexOf(substr, [start]) 方法搜索并(如果找到)返回字符串中搜索到的字符或子字符串的索引。
+// 如果没有找到，则返回-1。Start是一个可选参数，指定字符串中开始搜索的位置，默认值为0。
+// lastIndexOf(substr, [start])返回指定文本在字符串中最后一次出现的索引, 如果未找到，
+//则返回-1。 “Start”是一个可选参数，指定字符串中开始搜索的位置, 默认值为string.length-1
+// match(regexp)据正则表达式在字符串中搜索匹配项。如果没有找到匹配项，则返回一个信息数组或null
+// replace(regexp/substr, replacetext) 方法用于在字符串中用一些字符替换另一些字符，或替换一个与正则表达式匹配的子串
+// search(regexp)方法用于检索字符串中指定的子字符串，或检索与正则表达式相匹配的子字符串，如果找到，返回与 regexp 相匹配的子串的起始位置，否则返回 -1
+// slice(start, [end])提取字符串的某个部分，返回一个新的字符串
+// split(delimiter, [limit])把一个字符串分割成字符串数组，返回一个字符串数组返回的数组中的字串不包括 delimiter自身。 
+//可选的“limit”是一个整数，允许各位指定要返回的最大数组的元素个数
+// substr(start, [length])start（包括 start 所指的字符） 处开始的 length 个字符。如果没有指定 length，那么返回的字符串包含从 start 到该字符串的结尾的字符
+// substring(from, [to])方法用于提取字符串中介于两个指定下标之间的字符，方返回的子串包括 start 处的字符，但不包括 stop 处的字符，to 可选，
+//如果省略该参数，那么返回的子串会一直到字符串的结尾。
+// toLowerCase()
+// toUpperCase()
+// includes()
+// endsWith()函数检查字符串是否以指定的字符串或字符结束
+// repeat() 构造并返回一个新字符串，该字符串包含被连接在一起的指定数量的字符串的副本
+// valueOf()方法返回一个String对象的原始值（primitive value）
+// trim()  方法会从一个字符串的两端删除空白字符。在这个上下文中的空白字符是所有的空白字符
