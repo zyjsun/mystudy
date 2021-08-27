09b29062af72eb98c0194f94916c3b34d7d8ed1f
zyjsun <1821554162@qq.com>
2021-08-27 20:02:24 +0800

    zz

---
+++ interview/33/慕课网/js/1.js
@@ -0,0 +1,16 @@
+function myInstanceOf (left, right) {
+  let proto = left.__proto__
+  let prototype = right.prototype
+  while (true) {
+    if (proto == null) {
+      return false
+    } else {
+      if (proto == prototype) {
+        return true
+      }
+      proto = proto.__proto__
+    }
+  }
+}
+
+console.log(myInstanceOf('str', String));
